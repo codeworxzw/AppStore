b31e477e75efa6ed59cc6ad06b0197e063b9bf17
Ricardo <rijatru@gmail.com>
2016-03-25 12:16:37 -0500

    Deleted rx folder from master

--- app/src/main/java/com/ricardotrujillo/prueba/viewmodel/di/components/AppComponent.java
+++ app/src/main/java/com/ricardotrujillo/prueba/viewmodel/di/components/AppComponent.java
@@ -28,7 +28,7 @@ public interface AppComponent {
 
     void inject(StoreRecyclerViewAdapter storeRecyclerViewAdapter);
 
-    void inject(EntryViewModel entryViewModel); //a
+    void inject(EntryViewModel entryViewModel);
 
     void inject(NetWorker netWorker);
 
