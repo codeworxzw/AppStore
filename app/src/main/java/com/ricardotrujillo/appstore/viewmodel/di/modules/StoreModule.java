b258ea531b852f5ccaccd92fb75a5dc74f7f7389
Ricardo <rijatru@gmail.com>
2016-03-27 21:02:32 -0500

    Working Otto

--- app/src/main/java/com/ricardotrujillo/appstore/viewmodel/di/modules/StoreModule.java
+++ app/src/main/java/com/ricardotrujillo/appstore/viewmodel/di/modules/StoreModule.java
@@ -1,28 +1,19 @@
 package com.ricardotrujillo.appstore.viewmodel.di.modules;
 
 import com.ricardotrujillo.appstore.App;
+import com.ricardotrujillo.appstore.model.StoreManager;
 import com.ricardotrujillo.appstore.viewmodel.di.scopes.AppScope;
 
 import dagger.Module;
 import dagger.Provides;
 
-/**
- * Created by ricardo on 3/24/16 at 1:47 PM.
- */
 @Module
-public class AppModule {
-
-    App app;
-
-    public AppModule(App application) {
-
-        app = application;
-    }
+public class StoreModule {
 
     @Provides
     @AppScope
-    App providesApplication() {
+    StoreManager provideStoreManager(App app) {
 
-        return app;
+        return new StoreManager(app);
     }
 }
